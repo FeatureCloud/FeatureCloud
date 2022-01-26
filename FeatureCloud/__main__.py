d939e2d97bc79602e0fb2a9d184c50a08fd8cd40
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2022-01-26 15:18:36 +0100

    Removed cli.py, use @click in __main__.py

--- FeatureCloud/__main__.py
+++ FeatureCloud/__main__.py
@@ -1,26 +1,3 @@
-# import argparse
-import os
-import subprocess
-import sys
-
-# parser = argparse.ArgumentParser(description="For registering and testing your apps or using other apps, please visit "
-#                                              "our "
-#                                              "website: \n https://featurecloud.ai.\n And for more information about"
-#                                              " FeatureCloud architecture: \n"
-#                                              "The FeatureCloud AI Store for Federated Learning in Biomedicine and "
-#                                              "Beyond\n "
-#                                              "https://arxiv.org/abs/2105.05734 ",
-#                                  )
-# parser.add_argument("--echo", type=str, default=None, help="Get an input and print it!")
-# parser.add_argument('--delete', '--del', default=None, type=str, help='Delete a the given test run')
-# parser.add_argument('--info', type=str, default=None, help='Get details about the given test run')
-# parser.add_argument('--list', dest='list', help='List all test runs', action='store_true', default=False)
-# parser.add_argument('--logs', type=str, default=None, help='Get the logs of the given test runs')
-# parser.add_argument('--start', type=str, default=None, help='Start the given test run')
-# parser.add_argument('--stop', type=str, default=None, help='Stop the given test run')
-# parser.add_argument('--traffic', type=str, default=None, help='Show the traffic of a single test run')
-
-
 import click
 from sys import exit
 from FeatureCloud.cli.api import controller
@@ -246,30 +223,3 @@ def logs(controller_host: str, test_id: str or int, instance_id: str or int, fro
     else:
         click.echo(result['detail'])
         exit()
-
-# def fc_command():
-#     args = parser.parse_args()
-#
-#     python = sys.executable
-#     details = subprocess.check_output([python, '-m', 'pip', 'show', 'FeatureCloud'])
-#     path = None
-#     for pair in details.decode('utf-8').strip().split("\n"):
-#         if "location:" in pair.lower():
-#             path = pair.strip().split(":")[-1].strip()
-#     if path is None:
-#         raise KeyError("There is no location")
-#
-#     cli = f"{path}/FeatureCloud/cli/cli.py"
-#     options = vars(args)
-#     print(options[0])
-#     print(len(options))
-#     for k, v in vars(args).items():
-#         if v is not None:
-#             if k == 'echo':
-#                 print(v)
-#             elif k == 'list':
-#                 if v:
-#                     os.system(f'{python} {cli} {k}')
-#             else:
-#
-#                 os.system(f'{python} {cli} {k} {v}')
