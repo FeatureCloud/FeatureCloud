6079498903a40020d6fcbd1410ae97eb26df7899
Balazs Orban <balazs.orban@gnd.ro>
2022-02-07 13:28:56 +0200

    Logs with tail and log level filter

--- FeatureCloud/controller/controller_management.py
+++ FeatureCloud/controller/controller_management.py
@@ -1,3 +1,6 @@
+import json
+import time
+
 import click
 import docker
 import os
@@ -13,6 +16,9 @@ CONTROLLER_LABEL = "FCControllerLabel"
 DEFAULT_PORT = 8000
 DEFAULT_CONTROLLER_NAME = 'fc-controller'
 DEFAULT_DATA_DIR = 'data'
+LOG_FETCH_INTERVAL = 3  # seconds
+LOG_LEVEL_CHOICES = ['debug', 'info', 'warn', 'error', 'fatal']
+
 
 def check_docker_status():
     """Checks whether all docker-related components have been installed."""
@@ -22,8 +28,10 @@ def check_docker_status():
         click.echo("Docker daemon is not available")
         exit()
 
+
 def check_controller_prerequisites():
     check_docker_status()
+
 
 def start(name: str, port: int, data_dir: str):
     check_controller_prerequisites()
@@ -52,6 +60,7 @@ def start(name: str, port: int, data_dir: str):
     else:
         subprocess.call(['sh', start_script_path])
 
+
 def stop(name: str):
     check_controller_prerequisites()
     client = docker.from_env()
@@ -64,6 +73,7 @@ def stop(name: str):
         click.echo("Removing controller with name " + container.name)
         client.api.remove_container(container.id, v=True, force=True)
 
+
 def prune_controllers(name: str):
     check_controller_prerequisites()
     client = docker.from_env()
@@ -73,29 +83,44 @@ def prune_controllers(name: str):
 
     client.containers.prune(filters={"label": [CONTROLLER_LABEL]})
 
+
 def logs(name: str, tail: bool, log_level: str):
     # get controller address
     check_controller_prerequisites()
     client = docker.from_env()
+    host_port = 0
     try:
         container = client.containers.get(name)
         host_port = container.attrs['NetworkSettings']['Ports']['8000/tcp'][0]['HostPort']
     except docker.errors.NotFound:
         click.echo("Container not found")
+        exit()
 
     # Get logs content from controller
-    params = {'from': 0}
-    r = requests.get("http://localhost:" + host_port + "/logs/?from=0")
-    click.echo(r.text)
+    lines_fetched = 0
+    while True is True:
+        url = "http://localhost:" + host_port + "/logs/?from=" + str(lines_fetched)
+        r = requests.get(url)
+        logs_content = r.json()
+        lines_fetched += len(logs_content)
+        if len(logs_content) > 0:
+            logs_to_display = prepare_logs(logs_content, log_level)
+            if len(logs_to_display) > 0:
+                click.echo(logs_to_display)
+        if not tail:
+            break
+        time.sleep(LOG_FETCH_INTERVAL)
 
 
 def status(name: str):
     check_controller_prerequisites()
     click.echo(subprocess.check_output(['docker', 'ps', '--filter', 'name=' + name]))
 
+
 def ls():
     check_controller_prerequisites()
     click.echo(subprocess.check_output(['docker', 'ps', '--filter', 'label=' + CONTROLLER_LABEL]))
+
 
 def prepare_start_script(start_script_extension: str, name: str, port: int, data_dir: str):
     start_script_path = os.path.join(START_SCRIPT_LOCATION, START_SCRIPT_NAME + start_script_extension)
@@ -103,6 +128,10 @@ def prepare_start_script(start_script_extension: str, name: str, port: int, data
     # Read in the file
     with open(start_script_path, 'r') as file:
         start_script = file.read()
+
+    # Stop controllers with the same name
+    start_script = start_script.replace("kill " + DEFAULT_CONTROLLER_NAME, "kill " + name)
+    start_script = start_script.replace("rm " + DEFAULT_CONTROLLER_NAME, "rm " + name)
 
     if name != DEFAULT_CONTROLLER_NAME:
         click.echo("Changing default controller name to " + name)
@@ -128,3 +157,12 @@ def prepare_start_script(start_script_extension: str, name: str, port: int, data
         file.write(start_script)
 
     return start_script_path
+
+
+def prepare_logs(logs_json, log_level):
+    logs_content = ""
+    log_level_index = LOG_LEVEL_CHOICES.index(log_level)
+    for line in logs_json:
+        if LOG_LEVEL_CHOICES.index(line['level']) >= log_level_index:
+            logs_content += str(line) + os.linesep
+    return logs_content
