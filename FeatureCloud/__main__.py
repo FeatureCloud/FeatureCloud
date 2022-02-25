b82465af373ee921ade7ff9fbbbf71d45d7b9b12
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2022-02-25 10:46:31 +0100

    One controller for each WF

--- FeatureCloud/__main__.py
+++ FeatureCloud/__main__.py
@@ -114,27 +114,25 @@ def workflow() -> None:
 
 
 @workflow.command('start')
-@click.option('--controller-hosts', default='http://localhost:8000,',
+@click.option('--controller-host', default='http://localhost:8000',
               help='Address of your running controller instance.',
               required=True)
-@click.option('--wf-dir', default='/home/mohammad/PycharmProjects/FeatureCloud/Workflow',
-              help='full workflow file path',
+@click.option('--wf-dir', default='',
+              help='path to directory containing the workflow',
               required=True)
 @click.option('--wf-file', default='example_wf',
-              help='.py file name',
+              help='python file including the workflow',
               required=True)
-@click.option('--channels', default='local,',
+@click.option('--channel', default='local',
               help='The communication channel to be used. Can be local or internet.',
               required=True)
-@click.option('--query-intervals', default='1,',
+@click.option('--query-interval', default=1,
               help='The interval after how many seconds the status call will be performed.',
               required=True)
-def start_workflow(controller_hosts: str, wf_dir: str, wf_file: str, channels: str, query_intervals: str):
+def start_workflow(controller_host: str, wf_dir: str, wf_file: str, channel: str, query_interval: str):
     sys.path.append(wf_dir)
     workflow_class = importlib.import_module(wf_file)
-    wf = workflow_class.WorkFlow(controller_hosts=list(filter(None, controller_hosts.split(','))),
-                                 channels=list(filter(None, channels.split(','))),
-                                 query_intervals=list(filter(None, query_intervals.split(','))))
+    wf = workflow_class.WorkFlow(controller_host=controller_host, channel=channel, query_interval=query_interval)
     wf.register_apps()
     wf.run()
 
