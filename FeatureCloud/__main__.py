b2fef29f164cc8bb98c3600ad3cee240eba1d4f8
Mahdi <mahdi.kazemimajd@gmail.com>
2022-03-09 13:11:35 +0100

    Separate workflow subcommand

--- FeatureCloud/__main__.py
+++ FeatureCloud/__main__.py
@@ -7,26 +7,6 @@ def fc_cli() -> None:
     """FeatureCloud pip package"""
 
 
-@fc_cli.group("workflow")
-def workflow() -> None:
-    """Workflow related commands"""
-
-@workflow.command('workflow')
-@click.option('--controller-host', default='http://localhost:8000',
-              help='Address of your running controller instance.',
-              required=True)
-@click.option('--wf-dir', default='.,.',
-              help='workflow path',
-              required=True)
-@click.option('--channel', default='local',
-              help='The communication channel to be used. Can be local or internet.',
-              required=True)
-@click.option('--query-interval', default=2,
-              help='The interval after how many seconds the status call will be performed.',
-              required=True)
-def workflow(controller_host: str, wf_dir: str, channel: str, query_interval):
-    # from
-    pass
 
 @fc_cli.group("controller")
 def controller()-> None:
