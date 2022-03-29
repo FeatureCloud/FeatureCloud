63a450af65213e2fb089e03b03aef8f1917c7515
Mahdi <mahdi.kazemimajd@gmail.com>
2022-03-29 13:49:04 +0200

    Change the default template to the app-blank

--- FeatureCloud/api/imp/app/commands.py
+++ FeatureCloud/api/imp/app/commands.py
@@ -4,10 +4,10 @@ from urllib.parse import urljoin
 
 
 def create_link(template_name: str) -> str:
-    TEMPLATE_URL = 'https://github.com/FeatureCloud/app-template.git'
+    TEMPLATE_URL = 'https://github.com/FeatureCloud/app-blank.git'
     return urljoin(TEMPLATE_URL, template_name+'.git')
 
-def new(name: str, directory: str = '.', template_name: str = 'app-template') -> str:
+def new(name: str, directory: str = '.', template_name: str = 'app-blank') -> str:
     repo = git.Repo.clone_from(create_link(template_name), os.path.join(directory, name))
     repo.delete_remote('origin')
     msg = 'Ready to develop! Enjoy!'
