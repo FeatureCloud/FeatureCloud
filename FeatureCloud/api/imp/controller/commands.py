61c3bebad03c6b8ae8442d295c4f9a7ed811b692
Mahdi <mahdi.kazemimajd@gmail.com>
2022-04-14 09:26:46 +0200

    Remove some redundant code

--- FeatureCloud/api/imp/controller/commands.py
+++ FeatureCloud/api/imp/controller/commands.py
@@ -1,4 +1,4 @@
-
+import tqdm
 import json
 import time
 
@@ -10,9 +10,6 @@ import requests
 import subprocess
 from sys import exit
 
-START_SCRIPT_LOCATION = "./FeatureCloud/api/imp/controller/start_scripts"
-START_SCRIPT_DIR = "FeatureCloud"
-START_SCRIPT_NAME = "start_controller"
 CONTROLLER_IMAGE = "featurecloud.ai/controller"
 CONTROLLER_LABEL = "FCControllerLabel"
 DEFAULT_PORT = 8000
@@ -54,8 +51,8 @@ def start(name: str, port: int, data_dir: str):
 
     # Run start script
     pull_proc = client.api.pull(repository='featurecloud.ai/controller', stream=True)
-    for p in pull_proc:
-        print(p)
+    for p in tqdm.tqdm(pull_proc, desc='Downloading...'):
+        pass
     client.containers.run(
             CONTROLLER_IMAGE,
             detach=True,
@@ -70,7 +67,7 @@ def stop(name: str):
     check_controller_prerequisites()
     client = get_docker_client()
 
-    if len(name) == 0:
+    if name:
         name = DEFAULT_CONTROLLER_NAME
 
     # Removing controllers filtered by name
@@ -126,45 +123,6 @@ def status(name: str):
 def ls():
     check_controller_prerequisites()
     click.echo(subprocess.check_output(['docker', 'ps', '--filter', 'label=' + CONTROLLER_LABEL]))
-
-
-def prepare_start_script(start_script_extension: str, name: str, port: int, data_dir: str):
-    start_script_path = os.path.join(START_SCRIPT_LOCATION, START_SCRIPT_NAME + start_script_extension)
-
-    # Read in the file
-    with open(start_script_path, 'r') as file:
-        start_script = file.read()
-
-    # Stop controllers with the same name
-    start_script = start_script.replace("kill " + DEFAULT_CONTROLLER_NAME, "kill " + name)
-    start_script = start_script.replace("rm " + DEFAULT_CONTROLLER_NAME, "rm " + name)
-
-    if name != DEFAULT_CONTROLLER_NAME:
-        click.echo("Changing default controller name to " + name)
-        # For Windows
-        start_script = start_script.replace("controllerName=" + DEFAULT_CONTROLLER_NAME, "controllerName=" + name)
-        # For MacOS/Linux
-        start_script = start_script.replace("--name " + DEFAULT_CONTROLLER_NAME, "--name " + name)
-        start_script = start_script.replace("--controller-name=" + DEFAULT_CONTROLLER_NAME, "--controller-name=" + name)
-
-    if port != DEFAULT_PORT:
-        click.echo("Changing default port to " + str(port))
-        start_script = start_script.replace(str(DEFAULT_PORT) + ":", str(port) + ":")
-
-    if data_dir != DEFAULT_DATA_DIR:
-        click.echo("Changing default data dir to " + data_dir)
-        start_script = start_script.replace(DEFAULT_DATA_DIR, data_dir)
-
-    start_script_dir = os.path.join(tempfile.gettempdir(), START_SCRIPT_DIR)
-    if not os.path.exists(start_script_dir):
-        os.makedirs(start_script_dir)
-    start_script_path = os.path.join(start_script_dir, START_SCRIPT_NAME + start_script_extension)
-
-    # Write the file out again
-    with open(start_script_path, 'w') as file:
-        file.write(start_script)
-
-    return start_script_path
 
 
 def prepare_logs(logs_json, log_level):
