4ad885af449be7ff32c9a6012f6338e7f20b566c
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2022-05-05 17:39:24 +0200

    app build, publish, download, remove

--- FeatureCloud/api/cli/app/commands.py
+++ FeatureCloud/api/cli/app/commands.py
@@ -1,14 +1,49 @@
 import click
 from FeatureCloud.api.imp.app import commands
 
+
 @click.group("app")
 def app() -> None:
     """app related commands"""
+
 
 @app.command('new')
 @click.argument('name', type=click.Path(), nargs=1)
 @click.argument('directory', type=click.Path(), nargs=1, required=False)
 @click.option('--template-name', help='You can specify other template')
 def new(name: click.Path, directory: click.Path, template_name: str):
-    _, msg = commands.new(**{k: v for k , v in locals().items() if v})
+    _, msg = commands.new(**{k: v for k, v in locals().items() if v})
+    click.echo(msg)
+
+
+@app.command('build')
+@click.argument('path', type=click.Path(), default=".", required=False)
+@click.argument('image_name', type=str, default='', required=False)
+@click.argument('tag', type=str, default="latest", nargs=1, required=False)
+@click.argument('rm', type=bool, default=False, nargs=1, required=False)
+def build(path: click.Path, image_name: str, tag: str, rm: bool):
+    _, msg = commands.build(**{k: v for k, v in locals().items() if v})
+    click.echo(msg)
+
+
+@app.command('download')
+@click.argument('name', type=str, default=None, nargs=1, required=False)
+@click.argument('tag', type=str, default="latest", nargs=1, required=False)
+def download(name: str, tag: str):
+    _, msg = commands.download(**{k: v for k, v in locals().items() if v})
+    click.echo(msg)
+
+
+@app.command('publish')
+@click.argument('name', type=str, default=None, nargs=1, required=False)
+@click.argument('tag', type=str, default="latest", nargs=1, required=False)
+def publish(name: str, tag: str):
+    _, msg = commands.publish(**{k: v for k, v in locals().items() if v})
+    click.echo(msg)
+
+
+@app.command('remove')
+@click.argument('name', type=str, default=None, nargs=1, required=False)
+def remove(name: str):
+    _, msg = commands.remove(**{k: v for k, v in locals().items() if v})
     click.echo(msg)
--- FeatureCloud/api/imp/app/commands.py
+++ FeatureCloud/api/imp/app/commands.py
@@ -1,14 +1,102 @@
 import git
 import os
 from urllib.parse import urljoin
+import docker
+import tqdm
 
 
 def create_link(template_name: str) -> str:
     TEMPLATE_URL = 'https://github.com/FeatureCloud/'
-    return urljoin(TEMPLATE_URL, template_name+'.git')
+    return urljoin(TEMPLATE_URL, template_name + '.git')
+
 
 def new(name: str, directory: str = '.', template_name: str = 'app-blank') -> str:
+    """
+    Parameters
+    ----------
+    name: str
+        app name
+    directory: str
+        path to create the app's directory in
+    template_name: str
+        a template repository in FeatureCloud repositories
+
+    Returns
+    -------
+    (None, msg): msg: str
+
+    """
     repo = git.Repo.clone_from(create_link(template_name), os.path.join(directory, name))
     repo.delete_remote('origin')
     msg = 'Ready to develop! Enjoy!'
     return (None, msg)
+
+
+def build(path: str = ".", image_name: str = None, tag: str = "latest", rm: str = False):
+    """ Build app image,
+     Once on image_name is provided the name of the current directory will be used
+     Also, the current directory will be searched for the Dockerfile
+
+    Parameters
+    ----------
+    path: str
+        path to the directory containing the Dockerfile
+    image_name: str
+        name of the app's docker image
+    tag: str
+        tag for the image name
+    rm: bool
+        if True, remove intermediate containers
+    Returns
+    -------
+
+    """
+    if image_name is None:
+        image_name = os.getcwd().split("/")[-1]
+    client = docker.from_env()
+    build_proc = client.api.build(path=path, tag=f"{image_name}:{tag}", rm=rm)
+    log(build_proc, description=f"Building {image_name}:{tag} ...")
+    return None, None
+
+
+def download(name: str, tag: str = "latest"):
+    name, msg = fc_repo_name_check(name, tag)
+    if name is not None:
+        client = docker.from_env()
+        pull_proc = client.api.pull(name)
+        log(pull_proc, description=f"Downloading {name} ...")
+    return None, msg
+
+
+def fc_repo_name_check(name, tag):
+    msg = ""
+    if "featurecloud.ai/" not in name:
+        if "/" in name:
+            correct_name = f"featurecloud.ai/{name.strip().split('/')[-1]}:{tag}"
+            msg = f"The app docker image name {name}:{tag} is not correct.\n{correct_name} can be an acceptable name!"
+            return None, msg
+        name = f"featurecloud.ai/{name}"
+        msg = f"The complete name should include the FeatureCloud docker repository address." \
+              f"\nThe app name is completed: {name}\n"
+        return None, msg
+    return f"{name}:{tag}", msg
+
+
+def log(proc, description):
+    for _ in tqdm.tqdm(proc, desc=description):
+        pass
+
+
+def publish(name: str, tag: str = "latest"):
+    name, msg = fc_repo_name_check(name, tag)
+    if name is not None:
+        client = docker.from_env()
+        push_proc = client.api.push(repository=name, tag=tag)
+        log(push_proc, description=f"Uploading {name}:{tag} ...")
+    return None, msg
+
+
+def remove(name: str):
+    client = docker.from_env()
+    client.images.remove(image=name)
+    return None, None
