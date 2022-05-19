fefa9b584ca3e3bc1717c6be9fa560a2e2148f83
Balazs Orban <balazs.orban@gnd.ro>
2022-05-19 10:20:06 +0300

    Scaffold for pip package test cases

---
+++ tests/test_pip_package.py
@@ -0,0 +1,27 @@
+import os
+from unittest import TestCase
+from click.testing import CliRunner
+
+
+class TestPipPackageTestCase(TestCase):
+
+    def setUp(self):
+        self.runner = CliRunner()
+
+    def run_test(self):
+        pass
+
+    def start_controller(self):
+        pass
+
+    def run_dice_app(self):
+        pass
+
+    def check_logs(self):
+        pass
+
+    def stop_controller(self):
+        pass
+
+    def create_new_app(self):
+        pass
