9242494c9c9d2651a54b8eb2e64447e78cea8b77
Balazs Orban <balazs.orban@gnd.ro>
2022-06-17 13:29:06 +0300

    Removed try catches from tests, modified them to suit new app messages.

--- tests/test_pip_package.py
+++ tests/test_pip_package.py
@@ -23,57 +23,32 @@ class TestPipPackageTestCase(TestCase):
         self.create_new_app()
 
     def start_controller(self):
-        try:
-            output = subprocess.check_output(['featurecloud controller start'], shell=True)
-            assert output.find(b'Started controller: fc-controller') > -1
-        except subprocess.CalledProcessError as e:
-            print(e.returncode)
-            print(e.output)
+        output = subprocess.check_output(['featurecloud controller start'], shell=True)
+        assert output.find(b'Started controller: fc-controller') > -1
 
     def run_app(self, app_name):
-        try:
-            output = subprocess.check_output([
-                f'featurecloud test start --controller-host=http://localhost:8000 --client-dirs=. --generic-dir=. --app-image={app_name}'],
-                shell=True)
-            output = output.decode("utf-8")
-            assert output.find('started') > -1
-            output = output.replace('Test id=', '')
-            return output.replace(' started', '')
-        except subprocess.CalledProcessError as e:
-            print(e.returncode)
-            print(e.output)
+        output = subprocess.check_output([
+            f'featurecloud test start --controller-host=http://localhost:8000 --client-dirs=. --generic-dir=. --app-image={app_name}'],
+            shell=True)
+        output = output.decode("utf-8")
+        assert output.find('started') > -1
+        output = output.replace('Test id=', '')
+        return output.replace(' started', '')
 
     def check_logs(self, test_id):
-        try:
-            output = subprocess.check_output([
-                                                 f' featurecloud test logs --test-id={test_id}  --controller-host=http://localhost:8000 --instance-id=0 --from-param=0'],
-                                             shell=True)
-            assert output.find(b'Sent status') > -1
-        except subprocess.CalledProcessError as e:
-            print(e.returncode)
-            print(e.output)
+        check_logs_cmd = f' featurecloud test logs --instance-id=0 --from-row=0 --controller-host=http://localhost:8000 --test-id={test_id}'
+        output = subprocess.check_output([check_logs_cmd], shell=True)
+        assert output.find(b'CRIT Supervisor is running as root') > -1
 
     def stop_app(self, test_id):
-        try:
-            output = subprocess.check_output([f'featurecloud  test stop --test-id={test_id}'], shell=True)
-            assert output.find(b'stopped') > -1
-        except subprocess.CalledProcessError as e:
-            print(e.returncode)
-            print(e.output)
+        output = subprocess.check_output([f'featurecloud  test stop --test-id={test_id}'], shell=True)
+        assert output.find(b'stopped') > -1
 
     def stop_controller(self):
-        try:
-            output = subprocess.check_output(["featurecloud controller stop"], shell=True)
-            assert output.find(b'Stopped controller(s): fc-controller') > -1
-        except subprocess.CalledProcessError as e:
-            print(e.returncode)
-            print(e.output)
+        output = subprocess.check_output(["featurecloud controller stop"], shell=True)
+        assert output.find(b'Stopped controller(s): fc-controller') > -1
 
     def create_new_app(self):
-        try:
-            output = subprocess.check_output(["featurecloud app new my-blank-app --template-name=app-blank"],
-                                             shell=True)
-            assert output.find(b'Ready to develop! Enjoy!') > -1
-        except subprocess.CalledProcessError as e:
-            print(e.returncode)
-            print(e.output)
+        output = subprocess.check_output(["featurecloud app new my-blank-app --template-name=app-blank"],
+                                         shell=True)
+        assert output.find(b'Enjoy!') > -1
