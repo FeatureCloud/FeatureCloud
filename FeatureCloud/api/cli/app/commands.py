53529cc9edd6ea97f31308a87eab1fc4662fc50a
mohammad-bakhtiari <mohammad.bakhtiari.ai@gmail.com>
2022-09-07 12:15:51 +0200

    plot cmd chngd plot-states

--- FeatureCloud/api/cli/app/commands.py
+++ FeatureCloud/api/cli/app/commands.py
@@ -151,7 +151,7 @@ def remove(name: str, tag: str):
         click.echo(f'Error: {e}')
 
 
-@app.command('plot')
+@app.command('plot-states')
 @click.argument('path', type=click.Path(), default=".", required=False)
 @click.option('--package', default=".", help='package/s which include/s the states')
 @click.option('--states', default='main', help='python files containing the states seperated by comma')
